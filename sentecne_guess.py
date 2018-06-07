d3f4779e7612d22cf692e3d8f5e165d2dcefcf6a
Jhmaxwell12 <jhmaxwell12@gmail.com>
2018-06-07 16:18:04 -0500

    scripts

---
+++ sentecne_guess.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from sense_hat import SenseHat
+sense = SenseHat()
+import time
+
+speed=0
+s = "Hello World!" 
+r=(255, 255, 255)
+while i < 1
+    speed = speed + .05
+    sense.show_message(s, speed, text_colour=r)
+    
+
+
+
+
+
+
+
+sense.clear()
